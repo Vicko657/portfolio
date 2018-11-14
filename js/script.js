7ba19e1393db74d4fa6cd4fe7c9bf5501e880639
Vicko657 <victoriaolusegun@btinternet.com>
2018-11-14 21:27:02 +0000

    update

--- js/script.js
+++ js/script.js
@@ -34,3 +34,22 @@ $(document).ready(function(){
 es.onclick = function(){
   $('#window').fadeOut("slow");
 };
+
+$(document).ready(function(){function isScrolledIntoView(elem){
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+
+  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+
+}
+$(window).scroll(function(){
+  $('.scroll-animations .animated').each(function(){
+    if(isScrolledIntoView(this)=== true){
+      $(this).addClass('fadeInLeft');
+      }
+    });
+  });
+});
